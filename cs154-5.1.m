3e7b9641a53ed58ff818694ab8628d5b814dcb5a
Munchic <khoi.u.pham@gmail.com>
2021-02-09 08:38:11 -0800

    5.1

---
+++ cs154-5.1.m
@@ -0,0 +1,30 @@
+# problem 1a
+x = realmax
+y = realmin
+x / y
+
+# 1b 
+x = 0
+y = 0
+x / y
+
+# 1c 
+x = realmin
+y = realmax
+x / y
+
+# 1d
+x = 1
+y = 1 + realmin
+x / y
+
+# problem 2
+# minimum possible distance: eps
+# maximum possible distance (double): 
+
+# 1.8 & 10^308 = 2^1024
+# eps * 2^1024 = 3.6 * 10^292
+
+# problem 3
+# 32 = 2^5
+# eps * 2^5 = 2^6 * 10^{-16} = 6.4 * 10^-15
